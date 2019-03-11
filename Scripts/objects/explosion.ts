89903a1b37fb394fffb8deb876af2c05e9993ac5
deruip <aronly1997@hotmail.com>
2019-03-11 17:50:24 -0400

    Added explosion sound effect

--- Scripts/objects/explosion.ts
+++ Scripts/objects/explosion.ts
@@ -1,6 +1,8 @@
 module objects {
     export class Explosion extends objects.GameObject {
         // Variables
+        private explosionSFX: createjs.AbstractSoundInstance;
+
         // Constructor
         constructor(x: number, y: number) {
             super("Explosion");
@@ -10,6 +12,9 @@ module objects {
 
             this.scaleX *= 2;
             this.scaleY *= 2;
+
+            this.explosionSFX = createjs.Sound.play("explode");
+            this.explosionSFX.volume = 0.2;
         }
 
         // Methods
